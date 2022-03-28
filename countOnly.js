ef0c20c431b784ec63965448d480b3f8711d977a
Sneha Mahajan <sneh.km@gmail.com>
2022-03-28 15:56:08 -0400

    Adding final code for countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -7,23 +7,28 @@ const assertEqual = function (actual, expected) {
 };
 
 const countOnly = function (allItems, itemsToCount) {
-  let count = {};
+  let resultingCount = {};
 
-  for (key in itemsToCount) {
-    if (itemsToCount[key] === true) {
-      let countItem = 0;
-      for (element of allItems) {
-        if (key === element) {
-          countItem += 1;
-        }
-        if (countItem > 0) {
-          count[key] = countItem;
-        }
+
+  // Iterate over all elements of allItems
+  for (element of allItems) {
+
+    // Check if we need to count this element?
+    if (itemsToCount[element] === true) {
+
+      // Does this key/valye pair already exist?
+      // If yes, add 1 to it.
+      // If not, initiate it for a value of 1.
+      if (resultingCount[element]) {
+        resultingCount[element] += 1;
+      } else {
+        resultingCount[element] = 1;
       }
     }
   }
-
-  return count;
+  
+  // Return object with counted key/value pairs
+  return resultingCount;
 };
 
 const firstNames = [
@@ -44,6 +49,8 @@ const result1 = countOnly(firstNames, {
   Fang: true,
   Agouhanna: false,
 });
+
+console.log(result1);
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
