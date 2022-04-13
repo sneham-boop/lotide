9d6ba8e53f8522fe23a80d251e88bf4ee7395063
Sneha Mahajan <sneh.km@gmail.com>
2022-04-12 22:56:52 -0400

    Added refactored assertArraysEqual function and test code. Also, clea… …ned up some comments on other test functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,5 @@
-const eqArrays = (array1, array2) => {
-  let matchedResult = true;
-  for (let i = 0; i < array1.length; i++) {
-    // check each element
-    if (array1[i] !== array2[i]) {
-      matchedResult = false;
-    }
-  }
-  return matchedResult;
-};
+// Import functions
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (array1, array2) => {
   if (eqArrays(array1, array2) === true) {
@@ -17,4 +9,8 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 
-assertArraysEqual([4, 5, 6], [4, 5, 6]);
+module.exports = assertArraysEqual;
+
+
+
+
