9fb07b4011d4101c411674e6ac37219582d114be
Sneha Mahajan <sneh.km@gmail.com>
2022-04-12 22:38:40 -0400

    Refactored eqArrays and added test code

--- eqArrays.js
+++ eqArrays.js
@@ -1,35 +1,14 @@
-// Function implementation for assertEqual()
-// Checks if primitive values passed are equal
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🥵🥵🥵 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Function implementation for eqArrays()
 // Checks arrays for equality
 const eqArrays = (array1, array2) => {
   // Return false when lengths not equal
-  if (array1.length !== array2.length) {
-    return false;
-  }
+  if (array1.length !== array2.length) return false;
   // Check each element, return false if index and value don't match
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
+    if (array1[i] !== array2[i]) return false;
   }
   // Return true if all code was evaluated
   return true;
 };
 
-// Checks / tests
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+// Checks / tests
+const result1 = eqArrays([1, 2, 3], [1, 2, 3]); // => true
+const result2 = eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+const result3 = eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+const result4 = eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(result1, true); // => should PASS
+assertEqual(result2, false); // => should FAIL
+assertEqual(result3, true); // => should PASS
+assertEqual(result4, false); // => should FAIL
