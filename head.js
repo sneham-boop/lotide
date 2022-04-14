3a72b906560f9837f5af97a199a49bb9c2a3c215
Sneha Mahajan <sneh.km@gmail.com>
2022-04-14 17:43:29 -0400

    Removed other files already moved to utility functions directory

--- head.js
+++
@@ -1,5 +0,0 @@
-// Function implementation for head
-// head(array): Returns the first element of array.
-const head = (array) => array[0];
-
-module.exports = head;
--- tail.js
+++
@@ -1,5 +0,0 @@
-// Function implementation for tail
-// tail(array): Returns the array without its first element.
-const tail = (array) => array.slice(1);
-
-module.exports = tail;
