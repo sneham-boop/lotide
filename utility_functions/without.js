81d7860e5f310226ef3f3960febdd72b2dda4e48
Sneha Mahajan <sneh.km@gmail.com>
2022-04-14 23:06:45 -0400

    Fixed test code and function to include edge cases

--- utility_functions/without.js
+++ utility_functions/without.js
@@ -1,36 +1,23 @@
-// Import functions
 const assertArraysEqual = require("../assert_functions/assertArraysEqual");
 
 // Function implementation for without
 // without(array1, array2 ): Returns an array of elements in 
 // array1 without the elements in array2.
 const without = (array1, array2) => {
-  // Spread operator is used to copy orginal array
-  // without modifying it
-  let result = [...array1];
+  let result = [];
+  // Only filter elements that are not present
+  result = array1.filter(a=>{
+    return !array2.includes(a);
+  });
 
-  for (let i = 0; i < array2.length; i++) {
-    // Check is element to remove is present in original items
-    if (result.includes(array2[i])) {
-      // Get index of element to remove
-      let index = result.indexOf(array2[i]);
 
-      // Get elements up to index (not including as per slice)
-      // concat value of remaining array
-      // Result is saved
-      result = result
-        .slice(0, index)
-        .concat(result.slice(index + 1, result.length));
-    }
-  }
-
-  // Return the resulting array
   return result;
 };
 
 module.exports = without;
 
-//console.log(without([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5]));
+console.log(without([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 5, 6]));
+console.log(without([0, 1, 2, 3, 2, 1], [0, 1, 2, 3]));
 
 // test cases for without function
 const words = ["hello", "world", "lighthouse"];
